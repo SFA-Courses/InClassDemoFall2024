f8f684e0499971c685ef017d4aac62e8bd0a8815
becneljj <becneljj@sfasu.edu>
2024-09-02 13:39:02 -0500

    Demo Questtions

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -9,14 +9,54 @@ public class ArrayDemo {
         //int val = 0;
 
         // Problem 1
-        for (int i = 0; i < maxRow; i++) {
-            
-            System.out.println();
+        for (int i = 0; i < maxRow; i++) {          
             for(int j = 0; j < maxCol; j++) {
                 values[i][j] = j + 1;
-                System.out.print(values[i][j]);
             }
-            
         }
+
+        //  Problem 2
+        for (int row= 0; row <  maxRow; row++) {          
+            for(int col = 0; col < maxCol; col++) {
+                values[row][col] =  row + col;
+            }
+        }
+
+        // Problem 3
+        values = new int[5][5];
+       /*  for (int row= 0; row <  maxRow; row++) {          
+            for(int col = 0; col < maxCol; col++) {
+                if (col == row)
+                    values[row][col] = col +1;
+            }
+        }
+            */
+        
+        for (int i = 0; i < values.length; i++)
+            values[i][i] = i +1;
+
+        printArray(values);
+    }
+    /**
+     * 
+     * @param matrix - must be a square matrix (i.e. same number of rows and cols)
+     */
+    public static boolean isUpper(int[][] matrix) {
+        for (int row= 1; row <  matrix.length; row++) {          
+            for(int col = 0; col < row; col++) {
+                if (matrix[row][col] != 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printArray(int[][] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j =0; j < nums[0].length; j++)
+                System.out.print(nums[i][j] + " ");
+            System.out.println();
+        }
+
     }
 }
