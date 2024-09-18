eb475c4afd6de307f95d16ba7ae235597f9829b5
Jeremy Becnel <becneljj@sfasu.edu>
2024-09-18 12:28:49 -0500

    Update TestFriendList.java

--- TestFriendList.java
+++ TestFriendList.java
@@ -3,9 +3,9 @@ public class TestFriendList {
     public static void main(String[] args) throws ListException {
         FriendsList friendList = new FriendsList();
         System.out.println("Testing add: ");
-        friendList.add(0, new Friend("bobOO", false));
+        friendList.add(0, new Friend("bob", false));
         friendList.add(1, new Friend("Lezduit", true));
-        friendList.add(2, new Friend("XxxGodxxX", true));
+        friendList.add(2, new Friend("Joe", true));
         System.out.println(friendList.toString());
 
         System.out.println();
