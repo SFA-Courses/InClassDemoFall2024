0cace1a0424d408e4f9ed9c87c346048b0aa8868
becneljj <becneljj@sfasu.edu>
2024-10-22 10:34:49 -0500

    happy little trees

--- BST.java
+++ BST.java
@@ -1,4 +1,48 @@
 public class BST<T extends Comparable<T>> {
+    private T root;
+    private BST<T> left; // left subtree
+    private BST<T> right; // right subtree
+  
+    // construct an empty tree
+    public BST() {
+        this.root = null;
+        this.left = null;
+        this.right = null;
+    }
+
+    // construct a singleton tree (just has a root)
+    public BST(T rootItem) {
+        this.root = rootItem;
+        this.left = new BST<T>(); // empty subtree
+        this.right = new BST<T>(); // empty subtree
+    }
+
+    public boolean isEmpty() {
+        return (this.root == null);
+    }
+
+    public void removeAll() {
+        this.root = null; 
+        this.right = null;
+        this.left = null;
+    }
+
+    // return the number of elements in the tree
+    public int size() {
+        if (this.isEmpty())
+            return 0;
+        else // compute the size of the tree 
+            return 1 + this.left.size() + this.right.size();
+        
+    }
+
+    // find the height of the tree
+    public int height() {
+
+        
+    }
+
+
    
     public String toString() {
         StringBuilder buffer = new StringBuilder(50);
